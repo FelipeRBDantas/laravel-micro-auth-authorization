01f9ddaec075a422bbe2c8f2c9ccb7f988c15162
Felipe Dantas <feliperbdantas@outlook.com>
2021-11-02 19:02:20 -0300

    User Register and Auth is implemented

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,21 @@
 use App\Http\Controllers\Api\{
     UserController
 };
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/users', UserController::class);
+/**
+ * Auth and Register Routes
+ */
+
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::post('/auth', [AuthController::class, 'auth']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/users', UserController::class);
+});
 
 Route::get('/', function () {
     return response()->json(['message' => 'ok']);
